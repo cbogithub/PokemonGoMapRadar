5c351398c152a8dd7c6d4e6e582d0ff0c7b97b01
Saulo Damian <saulo@virtua.mx>
2016-07-20 11:01:19 -0500

    fix ports

--- index.php
+++ index.php
@@ -3,12 +3,28 @@
 		file_put_contents('coords.json', json_encode(array('location'=> $_GET['location'])));
 	}
 	if(isset($_GET['start'])){
-		$steps = '5';
+		$steps = '3';
 		if(isset($_GET['steps'])){
 			$steps = $_GET['steps'];
 		}
-		exec('example.py -u panferno44 -p hola45 -l -st '.$_GET['steps']);
+		$host = '127.0.0.1'
+		if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+			$host = '0.0.0.0';
+		}
+		$port = intval(file_get_contents('port.conf'));
+		$command = 'example.py -u panferno44 -p hola45 -l "durango" -H '.$host.' -P '.$port.' -st '.$steps;
+		if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+			$command = 'python '.$command;
+		}
+		exec($command);
+		setcookie("pGo", $port,  time()+600);
+		$port++;
+		file_put_contents('port.conf', $port);
 	}
-	header('Location: '.$_SERVER['HTTP_HOST'].':5000');
-	var_dump(json_decode(file_get_contents('coords.json'),true));
+	if(isset($_COOKIE['pGo'])){
+		header('Location: '.$_SERVER['HTTP_HOST'].':'.$_COOKIE['pGo']);	
+	}
+	else{
+		header('Location: '.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'?start');
+	}
 ?>
