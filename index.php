10f2cbf9d9ba83350e8e4263ec798c5d576eb664
Saulo Damian <saulo@virtua.mx>
2016-07-23 01:18:01 -0500

    asdf

--- index.php
+++ index.php
@@ -7,12 +7,56 @@
 	        exec($cmd . " > /dev/null &");   
 	    } 
 	}
-	function init_start(){
-		if(strpos($_SERVER['REQUEST_URI'], '?') !== false){
-			header('Location: http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'&start');	
+	function portInUse($port){
+		$connection = @fsockopen($_SERVER['HTTP_HOST'], $_COOKIE['pGo']);
+		return is_resource($connection);
+	}
+	$start = false;
+	$globalPort = false;
+	if(isset($_COOKIE['pGo']) && !portInUse($_COOKIE['pGo'])) {
+    	$start = true;
+	}
+	else if(!isset($_COOKIE['pGo'])){
+    	$start = true;
+	}
+	if($start) {
+		$steps = '3';
+		if(isset($_GET['steps'])){
+			$steps = $_GET['steps'];
+		}
+		$host = '127.0.0.1';
+		if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+			$host = '0.0.0.0';
+		}
+		$command = '';
+		if(isset($_COOKIE['pGoPro'])) {
+			$_GET['p'] = $_COOKIE['pGoPro'];
+		}
+		if(isset($_GET['p'])){
+			$globalPort = $_GET['p'];
+			$command = 'example.py -u panferno44 -p hola45 -l "durango" -H '.$host.' -P '.$_GET['p'].' -st '.$steps;
+			setcookie("pGo", $_GET['p'],  time()+2678400);
+			setcookie("pGoPro", $_GET['p'],  time()+2678400);
+			$_COOKIE['pGo'] = $_GET['p'];
 		}
 		else{
-			header('Location: http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'?start');	
+			$port = intval(file_get_contents('port.conf'));
+			if($port > 5500){
+				$port = 5000;
+				exec('pkill -f example.py');
+			}
+			$globalPort = $port;
+			$command = 'example.py -u panferno44 -p hola45 -l "durango" -H '.$host.' -P '.$port.' -st '.$steps;
+			setcookie("pGo", $port,  time()+600);
+			$_COOKIE['pGo'] = $port;
+			$port++;
+			file_put_contents('port.conf', $port);
+		}
+		if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
+			$command = 'python '.$command;
+		}
+		if(!portInUse($globalPort)){
+			execInBackground($command);	
 		}
 	}
 	if(isset($_GET['location']) && isset($_COOKIE['pGo'])){
@@ -22,56 +66,6 @@
 		}
 		$coords[$_COOKIE['pGo']] = $_GET['location'];
 		file_put_contents('coords.json', json_encode($coords));
-		header('Location: ' . $_SERVER['HTTP_REFERER']);
 	}
-	else{
-		if(isset($_GET['start'])){
-			$steps = '3';
-			if(isset($_GET['steps'])){
-				$steps = $_GET['steps'];
-			}
-			$host = '127.0.0.1';
-			if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
-				$host = '0.0.0.0';
-			}
-			$command = '';
-			if(isset($_COOKIE['pGoPro'])) {
-				$_GET['p'] = $_COOKIE['pGoPro'];
-			}
-			if(isset($_GET['p'])){
-				$command = 'example.py -u panferno44 -p hola45 -l "durango" -H '.$host.' -P '.$_GET['p'].' -st '.$steps;
-				setcookie("pGo", $_GET['p'],  time()+2678400);
-				setcookie("pGoPro", $_GET['p'],  time()+2678400);
-				$_COOKIE['pGo'] = $_GET['p'];
-			}
-			else{
-				$port = intval(file_get_contents('port.conf'));
-				if($port > 5015){
-					$port = 5000;
-					exec('pkill -f example.py');
-				}
-				$command = 'example.py -u panferno44 -p hola45 -l "durango" -H '.$host.' -P '.$port.' -st '.$steps;
-				setcookie("pGo", $port,  time()+600);
-				$_COOKIE['pGo'] = $port;
-				$port++;
-				file_put_contents('port.conf', $port);
-			}
-			if (strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
-				$command = 'python '.$command;
-			}
-			execInBackground($command);
-		}
-		if(isset($_COOKIE['pGo'])){
-			if(intval($_COOKIE['pGo']) > 5015 && !isset($_GET['p'])) {
-				init_start();
-			}
-			else{
-				sleep(5);
-				header('Location: http://'.$_SERVER['HTTP_HOST'].':'.$_COOKIE['pGo']);	
-			}
-		}
-		else{
-			init_start();
-		}
-	}
+	echo file_get_contents('templates/example_fullmap.html');
 ?>
